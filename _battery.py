c01b2f7e69d53f0e8a227fa56c738de1d48e55a4
tmcnugget <graycday@icloud.com>
2025-02-19 15:44:09 +0000

    Update _battery.py

--- _battery.py
+++ _battery.py
@@ -1,78 +1,58 @@
 import smbus
 import time
 
-class ADCDevice(object):
-    def __init__(self):
-        self.cmd = 0
-        self.address = 0
-        self.bus = smbus.SMBus(1)
+bus = smbus.SMBus(1)
 
-    def detectI2C(self, addr):
-        try:
-            self.bus.write_byte(addr, 0)
-            return True
-        except:
-            return False
+def detect_i2c(addr):
+    try:
+        bus.write_byte(addr, 0)
+        return True
+    except:
+        return False
 
-    def close(self):
-        self.bus.close()
+def analog_read_pcf8591(chn):
+    """Read analog value from PCF8591 (4 channels: 0-3)."""
+    return bus.read_byte_data(0x4F, 0x40 + chn)
 
-class PCF8591(ADCDevice):
-    def __init__(self):
-        super(PCF8591, self).__init__()
-        self.cmd = 0x40     # The default command for PCF8591 is 0x40.
-        self.address = 0x4f # 0x48 is the default I2C address for PCF8591 Module.
+def analog_write_pcf8591(value):
+    """Write DAC value to PCF8591."""
+    bus.write_byte_data(0x4F, 0x40, value)
 
-    def analogRead(self, chn): # PCF8591 has 4 ADC input pins, chn: 0,1,2,3
-        value = self.bus.read_byte_data(self.address, self.cmd + chn)
-        return value
+def analog_read_ads7830(chn):
+    """Read analog value from ADS7830 (8 channels: 0-7)."""
+    cmd = 0x84 | (((chn << 2 | chn >> 1) & 0x07) << 4)
+    return bus.read_byte_data(0x48, cmd)
 
-    def analogWrite(self, value): # Write DAC value
-        self.bus.write_byte_data(self.address, self.cmd, value)
+# Detect which ADC is available
+adc_flag = None
+if detect_i2c(0x4F):  # PCF8591 detected
+    adc_flag = False
+elif detect_i2c(0x48):  # ADS7830 detected
+    adc_flag = True
+else:
+    print("No correct I2C address found.\n"
+          "Please use 'i2cdetect -y 1' to check the I2C address!\n"
+          "Program exiting.")
+    exit(-1)
 
-class ADS7830(ADCDevice):
-    def __init__(self):
-        super(ADS7830, self).__init__()
-        self.cmd = 0x84
-        self.address = 0x48 # 0x4b is the default I2C address for ADS7830 Module.
+def get_analog(chn):
+    """Read analog value based on detected ADC."""
+    return analog_read_ads7830(chn) if adc_flag else analog_read_pcf8591(chn)
 
-    def analogRead(self, chn): # ADS7830 has 8 ADC input pins, chn: 0,1,2,3,4,5,6,7
-        value = self.bus.read_byte_data(self.address, self.cmd | (((chn << 2 | chn >> 1) & 0x07) << 4))
-        return value
+def get_voltage(bat):
+    """Calculate battery voltage from ADC readings."""
+    if adc_flag:
+        val0 = get_analog(0)
+        val1 = get_analog(4)
+    else:
+        val0 = get_analog(0)
+        val1 = get_analog(1)
 
-class ADC:
-    def __init__(self):
-        self.adcFlag = None
-        self.adc = ADCDevice()
-        if self.adc.detectI2C(0x4f):  # Detect the PCF8591.
-            self.adcFlag = False
-            self.adc = PCF8591()
-        elif self.adc.detectI2C(0x48):  # Detect the ADS7830.
-            self.adcFlag = True
-            self.adc = ADS7830()
-        else:
-            print("No correct I2C address found, \n"
-                  "Please use command 'i2cdetect -y 1' to check the I2C address! \n"
-                  "Program Exit. \n")
-            exit(-1)
+    battery1 = round(val0 / 255 * 5 * 3, 2)
+    battery2 = round(val1 / 255 * 5 * 3, 2)
 
-    def getA(self, chn): # getA stands for get analogue
-        return self.adc.analogRead(chn)
+    return battery1 if bat == 1 else battery2
 
-    def getV(self, bat): # getV stands for get voltage
-        if self.adcFlag:
-            val0 = self.getA(0)
-            val1 = self.getA(4)
-        else:
-            val0 = self.getA(0)
-            val1 = self.getA(1)
-
-        battery1 = round(val0 / 255 * 5 * 3, 2)
-        battery2 = round(val1 / 255 * 5 * 3, 2)
-        if bat == 1:
-            return battery1
-        if bat == 2:
-            return battery2
 
 import unicornhat as unicorn
 
@@ -112,18 +92,13 @@ def display_battery(red1, green1, h1, red2, green2, h2):
         unicorn.set_pixel(*pixel)
 
 if __name__ == '__main__':
-    adc = ADC()
     try:
         while True:
-            bat1 = adc.getV(1)
-            bat2 = adc.getV(2)
-            
-            # Show the battery icon
-            display_battery(255 - scaleRGB(bat1), scaleRGB(bat1), scaleHeight(bat1), 255 - scaleRGB(bat2), scaleRGB(bat2), scaleHeight(bat2))
+            display_battery(255 - scaleRGB(get_voltage(1)), scaleRGB(get_voltage(1)), scaleHeight(get_voltage(1)), 255 - scaleRGB(get_voltage(2)), scaleRGB(get_voltage(2)), scaleHeight(get_voltage(2)))
             unicorn.show()
             time.sleep(0.1)
     except KeyboardInterrupt:
-        adc.adc.close()
+        bus.close()
         print("\nEnd of program")
 
 
